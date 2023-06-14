7c5fe567c625c336273126663f0b9a7c155fcad4
Robert Snedeker <bmxmikey2@gmail.com>
2023-06-14 11:53:05 -0400

    Moved all constants to environment.php

--- Bootstrap/environment.php
+++ Bootstrap/environment.php
@@ -3,7 +3,8 @@
 use Core\Env;
 use Tnapf\Router\Router;
 
-$envFile = ROOT . '/.env';
+const API_ENV = API_ROOT . '/.env';
+const API_PREFIX = '/api';
 
-$env = file_exists($envFile) ? Env::createFromFile($envFile) : new Env();
+$env = file_exists(API_ENV) ? Env::createFromFile(API_ENV) : new Env();
 $env->router = new Router();
