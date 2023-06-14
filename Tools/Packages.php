e11aaf9757b7e20f63bccdddec9c58ded5a272dd
Robert Snedeker <bmxmikey2@gmail.com>
2023-06-14 12:12:03 -0400

    Used proper syntax, removed redundant function

--- Tools/Packages.php
+++ Tools/Packages.php
@@ -2,22 +2,19 @@
 # Dependencies
 
 <?php
-
-function decode_file(string $path): stdClass
-{
-    return json_decode(file_get_contents($path));
-}
-
-$composer = decode_file(__DIR__ . '/../composer.json');
+$composer = json_decode(file_get_contents(__DIR__ . '/../composer.json'));;
 
 ?>
-<?php foreach ($composer->require as $name => $version) { ?>
+<?php foreach ($composer->require as $name => $version): ?>
 * [<?= $name ?>: <?= $version ?>](<?= str_starts_with($name, 'php') ? 'https://www.php.net/downloads.php' : "https://packagist.org/packages/{$name}" ?>)
-<?php } ?>
+<?php endforeach; ?>
 
 # Dev Dependencies
 
-<?php foreach ($composer->{'require-dev'} as $name => $version) { ?>
+<?php foreach ($composer->{'require-dev'} as $name => $version): ?>
 * [<?= $name ?>: <?= $version ?>](<?= str_starts_with($name, 'php') ? 'https://www.php.net/downloads.php' : "https://packagist.org/packages/{$name}" ?>)
-<?php } ?>
-<?php file_put_contents(__DIR__ . '/packages.md', ob_get_clean()); ?>
+<?php
+endforeach;
+
+file_put_contents(__DIR__ . '/packages.md', ob_get_clean());
+?>
